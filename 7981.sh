e28d87b850e6ebe266f57c120be552ace64187cd
y9858 <53130835+y9858@users.noreply.github.com>
2026-05-05 11:32:59 +0800

    Update 7981.sh

--- 7981.sh
+++ 7981.sh
@@ -4,7 +4,7 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci-light/Makefile
 
 # 修改默认 IP
-sed -i 's/192.168.110.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # ttyd 免帐号登录
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
